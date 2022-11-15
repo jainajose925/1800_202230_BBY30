2eacac5a355789623a3d3012e2be4f2c330edae8
Jaina Jose <jainajose.0925@gmail.com>
2022-11-15 11:35:35 -0800

    weekly display functions properly now :,)

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,7 @@ insertName();
 //GET DATE
 const date = new Date();
 
-let day = date.getDay();
+var day = date.getDay();
 let dayNum = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
@@ -36,38 +36,54 @@ const months = ["january", "febuary", "march",
 //CREATES DOCUMENT NAME FOR CURRENT DAY
 var currentDate = day + "" + months[month] + "" + dayNum + "_" + year;
 
+function checkSunday() {
+  var temp = new Date(date.getTime());
+  while (temp.getDay() != 0) {
+    temp.setDate(temp.getDate() - 1);
+  }
+  temp.setHours(0,0,0,0);
+  return temp;
+}
 
 //CHANGE COLUMN COLOURS DEPENDING ON LOG
-//#1 FIND LAST SUNDAY!!
-//#2 GO THRU DATA FROM WEEK
 function weekLog() {
   firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         var uid = user.uid;
         var userHistoryRef = db.collection('users/' + uid + '/history');
-        var columnNum = 1;
         var columnSelect = "";
 
-        userHistoryRef.get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
+        //ORDER FROM MOST RECENT, LIMIT TO 7
+        userHistoryRef.orderBy("date", "desc").limit(7).get().then((querySnapshot) => {
+          querySnapshot.forEach(async (doc) => {
               if (doc.id == currentDate) {
                 logDone();
               }
-              
-              columnSelect = "col" + columnNum;
+              //CONVERT FIREBASE TIMESTAMP TO JS DATE OBJECT
+              const d = await doc.get("date");
+              var second = d.seconds;
+              var jsDate = new Date(second*1000);
 
-              if (doc.get("breakfast") == true) {
-                document.getElementById(columnSelect).style.backgroundColor = "#7ffaa0";
-              } else if (doc.get("breakfast") == false) {
-                document.getElementById(columnSelect).style.backgroundColor = "#f78691";
-              } else {
-                document.getElementById(columnSelect).style.backgroundColor = "lightgray";
+              //GET COLUMN ID
+              columnSelect = "col" + (parseInt(jsDate.getDay())+1);
+              if (jsDate >= checkSunday()) {
+                console.log(jsDate);
+                if (doc.get("breakfast") == true) {
+                  //CHANGE TO GREEN IF TRUE
+                  console.log("true");
+                  document.getElementById(columnSelect).style.backgroundColor = "#7ffaa0";
+                } else if (doc.get("breakfast") == false) {
+                  //CHANGE TO RED IF FALSE
+                  console.log("false");
+                  document.getElementById(columnSelect).style.backgroundColor = "#f78691";
+                } else {
+                  //KEEP LIGHT GRAY
+                  document.getElementById(columnSelect).style.backgroundColor = "lightgray";
+                }
+                console.log(columnSelect + "\n");
               }
-
-             columnNum++;
           });
         });
-
       } else {
         // User is signed out
         // ...
@@ -81,15 +97,15 @@ function undoLog(){
     document.getElementById("breakfastButtons").style.display = "flex";
     document.getElementById("question").style.display = "block";
     document.getElementById("undo").style.display = "none";
-    document.getElementById("main_body").style.gridTemplateRows = "50% 6% 40%";
+    document.getElementById("main_body").style.gridTemplateRows = "50% 6% 60%";
     document.getElementById(select).style.backgroundColor = "lightgray";
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
       if (user) {
         var uid = user.uid;
         var docHistoryRef = db.collection('users/' + uid + '/history').doc(currentDate);
 
-      docHistoryRef.delete().then(() => {
+      await docHistoryRef.delete().then(() => {
           console.log("Document successfully deleted!");
       }).catch((error) => {
           console.error("Error removing document: ", error);
@@ -116,15 +132,17 @@ function logDone() {
 function breakfastTrue() {
     console.log("breakfast selected");
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
           var uid = user.uid;
           var userHistoryRef = db.collection('users/' + uid + '/history');
           console.log(currentDate);
 
-          userHistoryRef.doc(currentDate).set({
-            breakfast: true
-        })
+          await userHistoryRef.doc(currentDate).set({
+            breakfast: true,
+            date: firebase.firestore.FieldValue.serverTimestamp()  
+        });
+
         logDone();
         weekLog();
         } else {
@@ -138,14 +156,15 @@ function breakfastTrue() {
 function breakfastFalse() {
     console.log("no breakfast today :(");
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
           var uid = user.uid;
           var userHistoryRef = db.collection('users/' + uid + '/history');
           console.log(currentDate);
 
-          userHistoryRef.doc(currentDate).set({
-            breakfast: false
+          await userHistoryRef.doc(currentDate).set({
+            breakfast: false,
+            date: firebase.firestore.FieldValue.serverTimestamp() 
         })
 
         logDone();
@@ -164,6 +183,11 @@ weekLog();
 //LOG BREAKFAST
 const eat = document.getElementById("ate");
 const noeat = document.getElementById("noeat");
+const undoo = document.getElementById("undo");
+
+undoo.addEventListener("click", function() {
+  undoLog()
+});
 
 noeat.addEventListener("click", function() {
     breakfastFalse()
